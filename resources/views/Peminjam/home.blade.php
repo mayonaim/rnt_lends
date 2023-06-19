804fc50f5750a6830a2075272524df260148e34a
Ryezeeee <sultanabdilah81@gmail.com>
2023-06-19 13:05:27 +0700

    dashboard

--- resources/views/Peminjam/home.blade.php
+++ resources/views/Peminjam/home.blade.php
@@ -13,70 +13,251 @@
 
             <!-- R&T Lends -->
             <div class="col-xl-3 col-md-6 mb-4">
-                <div class="card border-left-success shadow h-100 py-2">
+                <a href="{{ route('borrower.view_assets') }}" class="card border-left-success shadow h-100 py-2"
+                    style="text-decoration: none;">
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
                             <div class="col mr-2">
                                 <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                    Apa itu R&T Lends?</div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">Peminjaman Ruangan dan Alat</div>
+                                    <h5>Ingin Langsung Meminjam? Klik Disini!</h5>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                </a>
             </div>
+
             <!-- R&T Lends -->
             <div class="col-xl-3 col-md-6 mb-4">
-                <div class="card border-left-warning shadow h-100 py-2">
+                <a href="https://youtu.be/FgqwpCtZjIM" class="card border-left-warning shadow h-100 py-2"
+                    style="text-decoration: none;">
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
                             <div class="col mr-2">
                                 <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                    Apakah ada syarat tertentu untuk melakukan peminjaman?</div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">tentu saja ada</div>
+                                    <h5>Cara Melakukan Peminjaman? klik Disini!</h5>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                </a>
             </div>
 
             <!-- Pending Requests Card Example -->
             <div class="col-xl-3 col-md-6 mb-4">
-                <div class="card border-left-primary shadow h-100 py-2">
+                <a href="{{ route('borrower.view_borrowing_requests') }}" class="card border-left-primary shadow h-100 py-2"
+                    style="text-decoration: none;">
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
                             <div class="col mr-2">
                                 <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                    Apakah ada sanksi jika terjadi kerusakan pada ruangan dan alat?</div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">Akan ada sanksi, jika melakukan
-                                    kerusakan</div>
+                                    <h5>Lihat Status Peminjaman? klik DIsini!</h5>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                </a>
             </div>
+            <!-- Pending Requests Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <a href="{{ route('borrower.view_borrowing_requests') }}" class="card border-left-danger shadow h-100 py-2"
+                    style="text-decoration: none;">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
+                                    <h5>Tata Cara Melakukan Pengembalian. klik DIsini!</h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
         </div>
-
-
-        <!-- About Us -->
-        <div class="card shadow mb-4">
+        <div class="card mb-4">
             <div class="card-header py-3">
-                <h6 class="m-0 font-weight-bold text-primary">Tentang Kami
-                </h6>
+                <h5 class="m-0 font-weight-bold" style="color: #028B91;">Tentang Kami</h5>
             </div>
-            <div class="card-body">
-                <p>Kami mengerti bahwa mencari dan memesan ruangan atau alat bisa menjadi tugas yang melelahkan dan memakan
-                    waktu. Itulah sebabnya kami hadir untuk memberikan solusi cepat dan mudah untuk kebutuhan peminjaman
-                    Anda. Dengan platform peminjaman online yang modern dan inovatif, Anda dapat memesan ruangan atau alat
-                    dengan cepat dan mudah.</p>
-                <p class="mb-0">R&B LENDS adalah platform peminjaman ruangan dan alat yang bertujuan untuk memudahkan
-                    pengguna dalam mencari dan menggunakan ruangan atau alat yang mereka butuhkan. Dengan fitur pencarian
-                    yang mudah dan ketersediaan yang lengkap, kami menyediakan solusi untuk kebutuhan peminjaman sementara
-                    atau acara khusus Anda.</p>
+            <div class="card-body ">
+                <p>Selamat datang di RNT LENDS, RNT LENDS ialah aplikasi peminjaman ruangan dan alat praktikum
+                    jurusan infomatika yang dapat diandalkan. Kami menyediakan solusi inovatif untuk memudahkan
+                    Anda
+                    dalam mencari dan meminjam ruangan serta peralatan praktikum yang Anda butuhkan.</p>
+                <p>Kami memahami betapa melelahkannya mencari dan meminjam ruangan atau alat
+                    praktikum
+                    dengan cara yang lama. Oleh karena itu, RNT LENDS hadir untuk memberikan solusi yang mudah
+                    dan efisien bagi kebutuhan peminjaman Anda.</p>
+                <p>Dengan platform kami yang modern dan intuitif, Anda dapat dengan cepat dan mudah
+                    menjelajahi pilihan ruangan yang lengkap serta beragam peralatan praktikum yang kami
+                    sediakan.
+                    Kami menawarkan ruangan yang dilengkapi dengan baik dan berbagai peralatan praktikum yang
+                    lengkap, memastikan Anda memiliki segala yang dibutuhkan untuk sukses dalam kegiatan
+                    praktikum
+                    Anda.</p>
             </div>
         </div>
 
-    </div>
+
+        <!-- ======= Portfolio Section ======= -->
+
+        <div class="card">
+            <section id="portfolio" class="portfolio">
+                <div data-aos="fade-up">
+                    <div class="card-header py-3">
+                        <div class="section-title">
+                            <h4 class="m-0 font-weight-bold" style="color: #028B91;">Portfolio</h4>
+                        </div>
+                    </div>
+
+                    <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="300">
+
+                        <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+                            <div class="portfolio-wrap">
+                                <img src="assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <h4>App 1</h4>
+                                    <p>App</p>
+                                    <div class="portfolio-links">
+                                        <a href="assets/img/portfolio/portfolio-1.jpg" data-gallery="portfolioGallery"
+                                            class="portfolio-lightbox" title="App 1"><i class="bx bx-plus"></i></a>
+                                        <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+                            <div class="portfolio-wrap">
+                                <img src="assets/img/portfolio/portfolio-2.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <h4>Web 3</h4>
+                                    <p>Web</p>
+                                    <div class="portfolio-links">
+                                        <a href="assets/img/portfolio/portfolio-2.jpg" data-gallery="portfolioGallery"
+                                            class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>
+                                        <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+                            <div class="portfolio-wrap">
+                                <img src="assets/img/portfolio/portfolio-3.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <h4>App 2</h4>
+                                    <p>App</p>
+                                    <div class="portfolio-links">
+                                        <a href="assets/img/portfolio/portfolio-3.jpg" data-gallery="portfolioGallery"
+                                            class="portfolio-lightbox" title="App 2"><i class="bx bx-plus"></i></a>
+                                        <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+                            <div class="portfolio-wrap">
+                                <img src="assets/img/portfolio/portfolio-4.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <h4>Card 2</h4>
+                                    <p>Card</p>
+                                    <div class="portfolio-links">
+                                        <a href="assets/img/portfolio/portfolio-4.jpg" data-gallery="portfolioGallery"
+                                            class="portfolio-lightbox" title="Card 2"><i class="bx bx-plus"></i></a>
+                                        <a href="portfolio-details.html" title="More Details"><i
+                                                class="bx bx-link"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+                            <div class="portfolio-wrap">
+                                <img src="assets/img/portfolio/portfolio-5.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <h4>Web 2</h4>
+                                    <p>Web</p>
+                                    <div class="portfolio-links">
+                                        <a href="assets/img/portfolio/portfolio-5.jpg" data-gallery="portfolioGallery"
+                                            class="portfolio-lightbox" title="Web 2"><i class="bx bx-plus"></i></a>
+                                        <a href="portfolio-details.html" title="More Details"><i
+                                                class="bx bx-link"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+                            <div class="portfolio-wrap">
+                                <img src="assets/img/portfolio/portfolio-6.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <h4>App 3</h4>
+                                    <p>App</p>
+                                    <div class="portfolio-links">
+                                        <a href="assets/img/portfolio/portfolio-6.jpg" data-gallery="portfolioGallery"
+                                            class="portfolio-lightbox" title="App 3"><i class="bx bx-plus"></i></a>
+                                        <a href="portfolio-details.html" title="More Details"><i
+                                                class="bx bx-link"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+                            <div class="portfolio-wrap">
+                                <img src="assets/img/portfolio/portfolio-7.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <h4>Card 1</h4>
+                                    <p>Card</p>
+                                    <div class="portfolio-links">
+                                        <a href="assets/img/portfolio/portfolio-7.jpg" data-gallery="portfolioGallery"
+                                            class="portfolio-lightbox" title="Card 1"><i class="bx bx-plus"></i></a>
+                                        <a href="portfolio-details.html" title="More Details"><i
+                                                class="bx bx-link"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+                            <div class="portfolio-wrap">
+                                <img src="assets/img/portfolio/portfolio-8.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <h4>Card 3</h4>
+                                    <p>Card</p>
+                                    <div class="portfolio-links">
+                                        <a href="assets/img/portfolio/portfolio-8.jpg" data-gallery="portfolioGallery"
+                                            class="portfolio-lightbox" title="Card 3"><i class="bx bx-plus"></i></a>
+                                        <a href="portfolio-details.html" title="More Details"><i
+                                                class="bx bx-link"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+                            <div class="portfolio-wrap">
+                                <img src="assets/img/portfolio/portfolio-9.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <h4>Web 3</h4>
+                                    <p>Web</p>
+                                    <div class="portfolio-links">
+                                        <a href="assets/img/portfolio/portfolio-9.jpg" data-gallery="portfolioGallery"
+                                            class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>
+                                        <a href="portfolio-details.html" title="More Details"><i
+                                                class="bx bx-link"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </section><!-- End Portfolio Section -->
+        </div>
     </div>
 
     </div>
